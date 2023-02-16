07c6bd1df0b64650817ea444535d81e93e4149a2
RuperthQ <ruper111@hotmail.com>
2023-02-15 20:30:45 -0500

    Cards OK

--- contenido.php
+++ contenido.php
@@ -1,25 +1,25 @@
-<div class="card" style="width: 18rem;">
-  <img src="https://socialmedier.com/wp-content/uploads/2023/01/marketing-digital-chatgpt-0.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Chat GPT</h5>
-    <p class="card-text">ChatGPT es un prototipo de chatbot de inteligencia artificial desarrollado en 2022 por OpenAI que se especializa en el diálogo. El chatbot es un gran modelo de lenguaje ajustado con técnicas de aprendizaje tanto supervisadas como de refuerzo.​ Se basa en el modelo GPT-3.5 de OpenAI, una versión mejorada de GPT-3.</p>
-    <a href="https://chat.openai.com/chat" class="btn btn-primary">Conocer Mas</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="https://articles-img.sftcdn.net/f_auto,t_article_cover_xl/auto-mapping-folder/sites/2/2023/02/google-bard-febrero-9.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Bard - Google</h5>
-    <p class="card-text">Vamos a explicarte qué es y qué ofrece Bard, el chat por inteligencia artificial creado por Google. Se trata de una alternativa a ChatGPT y ChatGPT Plus con la que la empresa del buscador pretende tener un competidor propio en este campo. La idea sigue siendo que le preguntes cosas y te ofrezca las respuestas sin tener que entrar en otra web.
-    Google Bard no ha sido lanzado de forma oficial, por lo que todavía no tenemos toda la información sobre lo que es capaz de hacer. Sin embargo, vamos a decirte en este artículo todo lo que sabemos de la IA con la que Google competirá con ChatGPT, y cuando vayamos sabiendo más detalles lo actualizaremos.</p>
-    <a href="https://www.elespanol.com/elandroidelibre/noticias-y-novedades/20230213/ia-google-convierte-hazmerreir-empleados-compania/741175878_0.html" class="btn btn-primary">Conocer Mas</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container p-5 text-black">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="card h-100 shadow">
+        <img src="https://socialmedier.com/wp-content/uploads/2023/01/marketing-digital-chatgpt-0.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Chat GPT</h5>
+          <p class="card-text">ChatGPT es un prototipo de chatbot de inteligencia artificial desarrollado en 2022 por OpenAI que se especializa en el diálogo. El chatbot es un gran modelo de lenguaje ajustado con técnicas de aprendizaje tanto supervisadas como de refuerzo.​ Se basa en el modelo GPT-3.5 de OpenAI, una versión mejorada de GPT-3.</p>
+          <a href="https://chat.openai.com/chat" class="btn btn-primary">Conocer Mas</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card h-100 shadow">
+        <img src="https://articles-img.sftcdn.net/f_auto,t_article_cover_xl/auto-mapping-folder/sites/2/2023/02/google-bard-febrero-9.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Bard - Google</h5>
+          <p class="card-text">Vamos a explicarte qué es y qué ofrece Bard, el chat por inteligencia artificial creado por Google. Se trata de una alternativa a ChatGPT y ChatGPT Plus con la que la empresa del buscador pretende tener un competidor propio en este campo. La idea sigue siendo que le preguntes cosas y te ofrezca las respuestas sin tener que entrar en otra web.
+            Google Bard no ha sido lanzado de forma oficial, por lo que todavía no tenemos toda la información sobre lo que es capaz de hacer. Sin embargo, vamos a decirte en este artículo todo lo que sabemos de la IA con la que Google competirá con ChatGPT, y cuando vayamos sabiendo más detalles lo actualizaremos.</p>
+          <a href="https://www.elespanol.com/elandroidelibre/noticias-y-novedades/20230213/ia-google-convierte-hazmerreir-empleados-compania/741175878_0.html" class="btn btn-primary">Conocer Mas</a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
