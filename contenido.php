4eae2fd8cc47616f46c37a1dfd98967b97ff6718
RuperthQ <ruper111@hotmail.com>
2023-02-15 20:06:20 -0500

    chat gpt

--- contenido.php
+++ contenido.php
@@ -1,9 +1,9 @@
 <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Chat GPT</h5>
+    <p class="card-text">ChatGPT es un prototipo de chatbot de inteligencia artificial desarrollado en 2022 por OpenAI que se especializa en el diálogo. El chatbot es un gran modelo de lenguaje ajustado con técnicas de aprendizaje tanto supervisadas como de refuerzo.​ Se basa en el modelo GPT-3.5 de OpenAI, una versión mejorada de GPT-3.</p>
+    <a href="https://chat.openai.com/chat" class="btn btn-primary">Ir</a>
   </div>
 </div>
 <div class="card" style="width: 18rem;">
